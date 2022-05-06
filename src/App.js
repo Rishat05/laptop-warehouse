f9438ae86922b6a766e115b0114c2ead2a4bfe90
rishat <rishatrifa56@gmail.com>
2022-05-07 02:39:37 +0600

    footer done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './component/pages/Home/Home';
+import Footer from './component/Shared/Footer/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/component/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='p-3 text-center bg-secondary text-white'>
+            <p>copyright @ 2022</p>
+        </div>
+    );
+};
+
+export default Footer;
